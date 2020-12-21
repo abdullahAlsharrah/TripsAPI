80dc644f94149a86007327b6d6fc5d53ac0f67fd
abdullahAlsharrah <adbdb.27@gmaail.com>
2020-12-21 20:15:19 +0300

    user

--- app.js
+++ app.js
@@ -1,10 +1,40 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const slugify = require("slugify");
-
+const db = require("./db/models");
 const app = express();
+const { localStrategy } = require("./middleware/passport");
+const userRoute = require("./routes/users");
+
+// middlewares
+const passport = require("passport");
 
 app.use(bodyParser.json());
-
 app.use(cors());
+app.use(passport.initialize());
+passport.use(localStrategy);
+app.use(userRoute);
+
+app.use((req, res, next) => {
+  res.status(404).json({ message: "Path not found" });
+});
+
+// ERROR HANDELING
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message || "Internal Server Error",
+  });
+});
+
+const run = async () => {
+  try {
+    await db.sequelize.sync();
+    app.listen(8002, () => {
+      console.log("Hello the app is succesfully working");
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+run();
---
+++ db/models/User.js
@@ -0,0 +1,31 @@
+const sequelize = require("sequelize");
+
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define("User", {
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: {
+        args: true,
+        msg: "Username already exists",
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+  return User;
+};
